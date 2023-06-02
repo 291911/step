5b1f5bace60162a67d8e8b87800a653fbeec52aa
291911 <105618550+291911@users.noreply.github.com>
2023-06-02 13:58:59 +0900

    Add files via upload

---
+++ 약수, 배수와 소수/배수와약수.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        A, B = map(int, input().split())
+        if A == 0 and B == 0:
+            break
+        elif B > A and B % A == 0:
+            print('factor')
+        elif A > B and A % B == 0:
+            print('multiple')
+        elif A % B != 0 and B % A != 0:
+            print('neither')
+    except:
+        break
+
