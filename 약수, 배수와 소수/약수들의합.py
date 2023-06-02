e889c07031ed1e1fc3c274c8658c5cb194a341a8
291911 <105618550+291911@users.noreply.github.com>
2023-06-02 14:19:23 +0900

    Add files via upload

---
+++ 약수, 배수와 소수/약수들의합.py
@@ -0,0 +1,13 @@
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    A = []
+    for i in range(1, n):
+        if n % i == 0:
+            A.append(i)
+        
+    if sum(A) == n:
+        print(n, ' = ', ' + '.join(str(i) for i in A), sep="")
+    else:
+        print(n, "is NOT perfect.")
